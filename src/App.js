4abadf1432d621b8061a77ed497cc95511a8a974
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-06-17 00:40:48 +0530

    created the routes with id and error page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SingleMovie from "./SingleMovie";
+import Error from "./Error";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="movie/:id" element={<SingleMovie />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
+};
 
 export default App;
