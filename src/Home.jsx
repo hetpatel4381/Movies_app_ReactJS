66d4bc078ccb13c131f8cafdca74cc1b4aa16066
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-06-19 13:14:25 +0530

    created the context api state management

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
+  const name = useGlobalContext();
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>My Home Page</div>
+      <p>{name}</p>
+    </>
+  );
 };
 
 export default Home;
